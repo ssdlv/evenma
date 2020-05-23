8423c1e52a40f9c77997949faa9d827e11033547
ssdlv <mspeedy733@gmail.com>
2020-05-23 06:58:15 +0200

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //URL::forceScheme ('https');
-        if($this->app->environment('production')) {
+        /*if($this->app->environment('production')) {
             URL::forceScheme('https');
-        }
+        }*/
 
         Schema::defaultStringLength(191);
     }
